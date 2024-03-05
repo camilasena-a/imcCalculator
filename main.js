0d7348f196c5eebe9046e0486c2b566b78e310a9
Camila Sena Araújo <camilasenaraujo@gmail.com>
2024-03-05 15:56:05 -0300

    iniciando js

--- main.js
+++ main.js
@@ -6,30 +6,32 @@ let modalButton = window.document.querySelector("#close-button")
 let modalBoard = window.document.querySelector(".modal-wrapper")
 let textResult = window.document.querySelector("#textResult")
 
-form.onsubmit = (event) => {
-    event.preventDefault()
-    const weight = inputWeight.value;
-    const height = inputHeight.value;
 
-    const result = IMCCalculator(weight,height)
-    addModal(result)
+function addModal() {
+    modalBoard.classList.toggle("open")
 }
 
 
-function addModal(imc) {
-    modalBoard.classList.add("open")
-    textResult.innerText = `Seu IMC é de ${imc}`
-}
-
-function closeModal() {
-    modalBoard.classList.remove("open")
-}
-
-modalButton.addEventListener('click', closeModal())
+submitButton.addEventListener('click', addModal())
 
 
-function IMCCalculator(weight, height) {
-    let imc = (weight/((height/100)**2)).toFixed(2);
-    return imc;
-}
+// function IMCCalculator(weight, height) {
+//     let imc = (weight/((height/100)**2)).toFixed(2);
+//     return imc;
+// }
 
+
+
+// form.onsubmit = (event) => {
+//     event.preventDefault()
+//     const weight = inputWeight.value;
+//     const height = inputHeight.value;
+
+//     const result = IMCCalculator(weight,height)
+//     addModal(result)
+// }
+
+
+// function closeModal() {
+//     modalBoard.classList.remove("open")
+// }
